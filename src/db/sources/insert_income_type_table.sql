8e69e7cb8be700a0a3b009c3222fc72c81154b84
Denys Shabelnyk <dionisiy1986@gmail.com>
2024-06-26 22:49:23 +0200

    #1 - insert for income_table

---
+++ src/db/sources/insert_income_type_table.sql
@@ -0,0 +1,5 @@
+INSERT INTO income_type(type) VALUES 'bonus';
+INSERT INTO income_type(type) VALUES 'salary';
+INSERT INTO income_type(type) VALUES 'travel_refund';
+INSERT INTO income_type(type) VALUES 'shop_refund';
+INSERT INTO income_type(type) VALUES 'service_refund';
