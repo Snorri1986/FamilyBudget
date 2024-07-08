bb8e50f70200ef56b89e84533c6f681b8bbc3038
Denys Shabelnyk <dionisiy1986@gmail.com>
2024-07-08 22:54:23 +0200

    #2 - add ( to the insert statement

--- src/db/sources/insert_currency_table.sql
+++ src/db/sources/insert_currency_table.sql
@@ -1,4 +1,4 @@
-INSERT INTO currency(name) VALUES 'UAH';
-INSERT INTO currency(name) VALUES 'DKK';
-INSERT INTO currency(name) VALUES 'USD';
-INSERT INTO currency(name) VALUES 'EUR';
+INSERT INTO currency(name) VALUES ('UAH');
+INSERT INTO currency(name) VALUES ('DKK');
+INSERT INTO currency(name) VALUES ('USD');
+INSERT INTO currency(name) VALUES ('EUR');
