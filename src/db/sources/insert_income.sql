87f92f0a2d15369dcb9f9d1941966608fc023a1f
Denys Shabelnyk <dionisiy1986@gmail.com>
2024-07-09 21:11:12 +0200

    #2 - insert statement to income table

---
+++ src/db/sources/insert_income.sql
@@ -0,0 +1,5 @@
+INSERT INTO income(i_type,amount,currency,date,target_card,comments)
+VALUES ('salary',5000,3,CURRENT_TIMESTAMP,'1234','salary for august 2024');
+
+INSERT INTO income(i_type,amount,currency,date,target_card,comments)
+VALUES ('bonus',10000,3,CURRENT_TIMESTAMP,'1234','annual bonus');
