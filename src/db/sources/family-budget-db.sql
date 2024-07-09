d1d109ea3bf0a6506262d7dd5490ebd2ec11635e
Denys Shabelnyk <dionisiy1986@gmail.com>
2024-07-09 21:20:32 +0200

    #2 - new references with income_type and currency tables

--- src/db/sources/family-budget-db.sql
+++ src/db/sources/family-budget-db.sql
@@ -94,9 +94,9 @@ CREATE TABLE IF NOT EXISTS "income_type" (
 );
 
 
-ALTER TABLE "income" ADD CONSTRAINT "income_fk1" FOREIGN KEY ("i_type") REFERENCES "income_type"("inc_type");
+ALTER TABLE "income" ADD CONSTRAINT "income_fk1" FOREIGN KEY ("i_type") REFERENCES "income_type"("id");
+ALTER TABLE "income" ADD CONSTRAINT "income_fk3" FOREIGN KEY ("currency") REFERENCES "currency"("id");
 
--- ALTER TABLE "income" ADD CONSTRAINT "income_fk3" FOREIGN KEY ("currency") REFERENCES "currency"("id");
 ALTER TABLE "groceries" ADD CONSTRAINT "groceries_fk1" FOREIGN KEY ("g_type") REFERENCES "expenses_type"("e_type");
 
 ALTER TABLE "groceries" ADD CONSTRAINT "groceries_fk3" FOREIGN KEY ("currency") REFERENCES "currency"("name");
