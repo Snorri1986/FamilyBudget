1d9400f048e3ce6c3b9088f3bed8da04a3d0ba5c
Denys Shabelnyk <dionisiy1986@gmail.com>
2024-09-09 22:21:42 +0200

    #8 - main page controller prototype

--- src/main/java/org/snorri1986/familybud/controller/MainPageController.java
+++ src/main/java/org/snorri1986/familybud/controller/MainPageController.java
@@ -1,4 +1,16 @@
 package org.snorri1986.familybud.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class MainPageController {
+
+  @GetMapping("/main")
+  public String getMainPage(Model model) {
+    //TODO: temp attribute.
+    model.addAttribute("the Date",java.time.LocalDateTime.now());
+    return "main";
+  }
 }
