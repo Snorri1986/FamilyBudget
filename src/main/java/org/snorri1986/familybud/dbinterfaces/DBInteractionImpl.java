64096dad75eb911a31ca2378959596c569723af1
Denys Shabelnyk <dionisiy1986@gmail.com>
2024-11-13 21:58:25 +0100

    #51 - Implementation for insertIncome()

--- src/main/java/org/snorri1986/familybud/dbinterfaces/DBInteractionImpl.java
+++ src/main/java/org/snorri1986/familybud/dbinterfaces/DBInteractionImpl.java
@@ -2,8 +2,10 @@ package org.snorri1986.familybud.dbinterfaces;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.Query;
 import org.snorri1986.familybud.models.*;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public class DBInteractionImpl implements DBInteraction {
@@ -12,8 +14,15 @@ public class DBInteractionImpl implements DBInteraction {
   private EntityManager entityManager;
 
   @Override
+  @Transactional
   public void insertIncome(IncomeModel income) {
-
+    Query query = entityManager.createNativeQuery("SELECT public.i_income(:income_type, :amount_value, :currency_value, :oper_date, :payment_card, :comm_value)");
+    query.setParameter("income_type", income.getIncomeType());
+    query.setParameter("amount_value", income.getAmount());
+    query.setParameter("currency_value", income.getCurrency());
+    query.setParameter("oper_date", income.getTransactionDate());
+    query.setParameter("payment_card", income.getCardNum());
+    query.setParameter("comm_value", income.getOperDescription());
   }
 
   @Override
