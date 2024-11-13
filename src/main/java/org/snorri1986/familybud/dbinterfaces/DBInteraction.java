fe34456ebb4d2699144be2215933514df36cda0b
Denys Shabelnyk <dionisiy1986@gmail.com>
2024-11-13 21:41:38 +0100

    #51 - Interface for DB interaction

---
+++ src/main/java/org/snorri1986/familybud/dbinterfaces/DBInteraction.java
@@ -0,0 +1,13 @@
+package org.snorri1986.familybud.dbinterfaces;
+
+import org.snorri1986.familybud.models.*;
+
+public interface DBInteraction {
+  void insertIncome(IncomeModel income);
+  void insertEntertainment(EntertainmentModel entertainmentModel);
+  void insertGroceries(GroceriesModel groceriesModel);
+  void insertHealth(HealthModel healthModel);
+  void insertRentHousing(RentHousingModel rentHousingModel);
+  void insertTelecom(TelecomModel telecomModel);
+  void insertTravel(TravelModel travelModel);
+}
