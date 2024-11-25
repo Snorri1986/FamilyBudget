01790a662e75bf4dba620adf1e4b831e99f63674
Denys Shabelnyk <dionisiy1986@gmail.com>
2024-11-25 22:35:53 +0100

    #53 - new service method to add groceries

--- src/main/java/org/snorri1986/familybud/service/DBService.java
+++ src/main/java/org/snorri1986/familybud/service/DBService.java
@@ -1,6 +1,7 @@
 package org.snorri1986.familybud.service;
 
 import org.snorri1986.familybud.models.EntertainmentModelDB;
+import org.snorri1986.familybud.models.GroceriesModelDB;
 import org.snorri1986.familybud.models.IncomeModelDB;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -31,5 +32,16 @@ public class DBService {
             entertainmentModelDB.getTransactionDate(),
             entertainmentModelDB.getCardNum(),
             entertainmentModelDB.getOperDescription()},String.class);
+  }
+
+  public void insertNewGroceries(GroceriesModelDB groceriesModelDB) {
+    String sql = "SELECT public.i_groceries(?,?,?,?,?,?)";
+    jdbcTemplate.queryForObject(sql, new Object[]{groceriesModelDB.getPurchesType(),
+            groceriesModelDB.getAmount(),
+            groceriesModelDB.getCurrency(),
+            groceriesModelDB.getTransactionDate(),
+            groceriesModelDB.getCardNum(),
+            groceriesModelDB.getOperDescription()},String.class);
+
   }
 }
