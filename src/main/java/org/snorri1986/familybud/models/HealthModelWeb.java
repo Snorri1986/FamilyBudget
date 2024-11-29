5190ab939a86560b3b32dc4faf0b0fc4582371d4
Denys Shabelnyk <dionisiy1986@gmail.com>
2024-11-29 22:44:23 +0100

    #54 - for debuging data from web

--- src/main/java/org/snorri1986/familybud/models/HealthModelWeb.java
+++ src/main/java/org/snorri1986/familybud/models/HealthModelWeb.java
@@ -60,4 +60,16 @@ public class HealthModelWeb {
   public void setOperDescription(String operDescription) {
     this.operDescription = operDescription;
   }
+
+  @Override
+  public String toString() {
+    return "HealthModelWeb{" +
+            "healthOperType='" + healthOperType + '\'' +
+            ", amount=" + amount +
+            ", currency='" + currency + '\'' +
+            ", transactionDate=" + transactionDate +
+            ", cardNum=" + cardNum +
+            ", operDescription='" + operDescription + '\'' +
+            '}';
+  }
 }
