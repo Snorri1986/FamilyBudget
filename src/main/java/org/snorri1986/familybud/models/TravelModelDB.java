b501974295393e2837e803d3aafdc1a441c9b4d0
Denys Shabelnyk <dionisiy1986@gmail.com>
2024-12-03 21:16:39 +0100

    #57 - class to insert data to DB

---
+++ src/main/java/org/snorri1986/familybud/models/TravelModelDB.java
@@ -0,0 +1,72 @@
+package org.snorri1986.familybud.models;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.Date;
+
+public class TravelModelDB {
+  private int travelType;
+  private int amount;
+  private int currency;
+  @DateTimeFormat(pattern = "dd.MM.yyyy HH:mm")
+  private Date transactionDate;
+  private int cardNum;
+  private String destination;
+  private String operDescription;
+
+  public int getTravelType() {
+    return travelType;
+  }
+
+  public void setTravelType(int travelType) {
+    this.travelType = travelType;
+  }
+
+  public int getAmount() {
+    return amount;
+  }
+
+  public void setAmount(int amount) {
+    this.amount = amount;
+  }
+
+  public int getCurrency() {
+    return currency;
+  }
+
+  public void setCurrency(int currency) {
+    this.currency = currency;
+  }
+
+  public Date getTransactionDate() {
+    return transactionDate;
+  }
+
+  public void setTransactionDate(Date transactionDate) {
+    this.transactionDate = transactionDate;
+  }
+
+  public int getCardNum() {
+    return cardNum;
+  }
+
+  public void setCardNum(int cardNum) {
+    this.cardNum = cardNum;
+  }
+
+  public String getDestination() {
+    return destination;
+  }
+
+  public void setDestination(String destination) {
+    this.destination = destination;
+  }
+
+  public String getOperDescription() {
+    return operDescription;
+  }
+
+  public void setOperDescription(String operDescription) {
+    this.operDescription = operDescription;
+  }
+}
