ad3838efa8281fdba1b190bb566c9c59d61ac896
Denys Shabelnyk <dionisiy1986@gmail.com>
2024-12-23 21:30:48 +0100

    #76 - set the same resources as prod

--- src/test/java/org/snorri1986/familybud/controller/MainPageControllerTest.java
+++ src/test/java/org/snorri1986/familybud/controller/MainPageControllerTest.java
@@ -6,8 +6,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(MainPageController.class)
 public class MainPageControllerTest {
@@ -16,9 +15,10 @@ public class MainPageControllerTest {
   private MockMvc mockMvc;
 
   @Test
-  public void testGetMainPage() throws Exception {
-    mockMvc.perform(get("http://localhost:8080/main"))
-            .andExpect(status().isOk())
-            .andExpect(content().string("FamilyBudget Cloud App"));
+  void testGetMainPage() throws Exception {
+    mockMvc.perform(get("/main"))
+            .andExpect(status().isOk())                       // HTTP 200 status
+            .andExpect(view().name("main"))                  // Correct view name
+            .andExpect(model().attributeExists("theDate"));  // Model contains 'theDate'
   }
 }
