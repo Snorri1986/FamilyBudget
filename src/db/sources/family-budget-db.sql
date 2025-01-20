bfe76a5f0c23f2aba7ac9a28e39de362515d766e
Denys Shabelnyk <dionisiy1986@gmail.com>
2025-01-20 21:24:37 +0100

    #87 - operType type for Entertainment table

--- src/db/sources/family-budget-db.sql
+++ src/db/sources/family-budget-db.sql
@@ -53,6 +53,7 @@ CREATE TABLE IF NOT EXISTS "entertainment" (
 	"event_type_id" bigint NOT NULL,
 	"amount" bigint NOT NULL,
 	"currency" bigint NOT NULL,
+	"operType" varchar(255) NOT NULL,
 	"date" timestamp with time zone NOT NULL,
 	"source_card" bigint NOT NULL,
 	"comments" varchar(255) NOT NULL,
