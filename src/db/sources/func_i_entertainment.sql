131b88a94f9603148336785b7bc20b8d542bedd4
Denys Shabelnyk <dionisiy1986@gmail.com>
2025-01-24 21:23:51 +0100

    #87 - new version of functions on db side

--- src/db/sources/func_i_entertainment.sql
+++ src/db/sources/func_i_entertainment.sql
@@ -1,26 +1,21 @@
-CREATE OR REPLACE FUNCTION public.i_entertainment(
-    evn_type_id_val int,
-    amount_val int,
-    cur_value int,
-    oper_date date,
-    src_card int,
-    comm_val character
-)
- RETURNS integer
- LANGUAGE plpgsql
-AS $function$
-    DECLARE
+create function i_entertainment(evn_type_id_val integer, amount_val integer, cur_value integer, oper_date date, oper_type character, src_card integer, comm_val character) returns integer
+    language plpgsql
+as
+$$
+DECLARE
     last_id int;
 
-    BEGIN
+BEGIN
 
-    INSERT INTO entertainment(event_type_id,amount,currency,date,source_card,comments)
-    VALUES (evn_type_id_val,amount_val,cur_value,oper_date,src_card,comm_val);
+    INSERT INTO entertainment(event_type_id,amount,currency,date,source_card,comments,opertype)
+    VALUES (evn_type_id_val,amount_val,cur_value,oper_date,src_card,comm_val,oper_type);
 
     SELECT max(id) INTO last_id FROM entertainment;
 
     RETURN last_id;
 
-	END;
-$function$
-;
+END;
+$$;
+
+alter function i_entertainment(integer, integer, integer, date, char, integer, char) owner to u4cg7fn2s82n4v;
+
--- src/db/sources/func_i_groceries.sql
+++ src/db/sources/func_i_groceries.sql
@@ -1,25 +1,21 @@
-CREATE OR REPLACE FUNCTION public.i_groceries(
-gtype_value integer,
-amount_value integer,
-currency_value integer,
-oper_date date,
-src_payment_card integer,
-comm_value character)
- RETURNS integer
- LANGUAGE plpgsql
-AS $function$
-    DECLARE
+create function i_groceries(gtype_value integer, amount_value integer, currency_value integer, oper_date date, oper_type character, src_payment_card integer, comm_value character) returns integer
+    language plpgsql
+as
+$$
+DECLARE
     last_id int;
 
-    BEGIN
+BEGIN
 
-    INSERT INTO groceries(g_type, amount, currency, date, source_card, comments)
-    VALUES (gtype_value, amount_value,currency_value,oper_date,src_payment_card,comm_value);
+    INSERT INTO groceries(g_type, amount, currency, date, source_card, comments,opertype)
+    VALUES (gtype_value, amount_value,currency_value,oper_date,src_payment_card,comm_value,oper_type);
 
     SELECT max(id) INTO last_id FROM groceries;
 
     RETURN last_id;
 
-	END;
-$function$
-;
+END;
+$$;
+
+alter function i_groceries(integer, integer, integer, date, char, integer, char) owner to u4cg7fn2s82n4v;
+
