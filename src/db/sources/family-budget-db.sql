5568d729053240f6e118d33eb192f4ed5e8ccc8d
Denys Shabelnyk <dionisiy1986@gmail.com>
2025-01-27 21:59:25 +0100

    #87 - new field for rent_housing table

--- src/db/sources/family-budget-db.sql
+++ src/db/sources/family-budget-db.sql
@@ -31,6 +31,7 @@ CREATE TABLE IF NOT EXISTS "housing_rent" (
 	"hr_type_id" bigint NOT NULL,
 	"amount" bigint NOT NULL,
 	"currency" bigint NOT NULL,
+	"operType" varchar(255) NOT NULL,
 	"date" timestamp with time zone NOT NULL,
 	"source_card" bigint NOT NULL,
 	"comments" varchar(255) NOT NULL,
