54c9ac9dbec1e3dff90efa420cc6bc620982e2c5
Denys Shabelnyk <dionisiy1986@gmail.com>
2025-01-27 21:39:04 +0100

    #87 - change db function for Health

--- src/db/sources/func_i_health.sql
+++ src/db/sources/func_i_health.sql
@@ -1,26 +1,21 @@
-CREATE OR REPLACE FUNCTION public.i_health(
-    h_type_id_val int,
-    amount_val int,
-    cur_val int,
-    oper_date date,
-    src_card int,
-    comm_val character
-)
- RETURNS integer
- LANGUAGE plpgsql
-AS $function$
-    DECLARE
+create function i_health(h_type_id_val integer, amount_val integer, cur_val integer, oper_date date, oper_type character, src_card integer, comm_val character) returns integer
+    language plpgsql
+as
+$$
+DECLARE
     last_id int;
 
-    BEGIN
+BEGIN
 
-    INSERT INTO health(h_type_id,amount,currency,date,source_card,comments)
-    VALUES (h_type_id_val,amount_val,cur_val,oper_date,src_card,comm_val);
+    INSERT INTO health(h_type_id,amount,currency,date,source_card,comments,opertype)
+    VALUES (h_type_id_val,amount_val,cur_val,oper_date,src_card,comm_val,oper_type);
 
     SELECT max(id) INTO last_id FROM health;
 
     RETURN last_id;
 
-	END;
-$function$
-;
+END;
+$$;
+
+alter function i_health(integer, integer, integer, date, char, integer, char) owner to u4cg7fn2s82n4v;
+
