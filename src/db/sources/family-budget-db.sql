b8de0b56f280f883dea7d629f39a7b280accf9d5
Denys Shabelnyk <dionisiy1986@gmail.com>
2025-01-29 20:37:22 +0100

    #87 - new column on table level for travel

--- src/db/sources/family-budget-db.sql
+++ src/db/sources/family-budget-db.sql
@@ -43,6 +43,7 @@ CREATE TABLE IF NOT EXISTS "travel" (
 	"tr_type_id" bigint NOT NULL,
 	"amount" bigint NOT NULL,
 	"currency" bigint NOT NULL,
+	"operType" varchar(255) NOT NULL,
 	"date" timestamp with time zone NOT NULL,
 	"source_card" bigint NOT NULL,
 	"destination" varchar(255) NOT NULL,
