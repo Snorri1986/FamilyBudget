d4e426f67819b662774a3c46a7294dcdc3e06f88
Denys Shabelnyk <dionisiy1986@gmail.com>
2025-01-31 20:57:46 +0100

    #101 - new i_entertainment without return types

--- src/db/sources/func_i_entertainment.sql
+++ src/db/sources/func_i_entertainment.sql
@@ -1,18 +1,12 @@
-create function i_entertainment(evn_type_id_val integer, amount_val integer, cur_value integer, oper_date date, oper_type character, src_card integer, comm_val character) returns integer
+create function i_entertainment(evn_type_id_val integer, amount_val integer, cur_value integer, oper_date date, oper_type character, src_card integer, comm_val character) returns void
     language plpgsql
 as
 $$
-DECLARE
-    last_id int;
 
 BEGIN
 
     INSERT INTO entertainment(event_type_id,amount,currency,date,source_card,comments,opertype)
     VALUES (evn_type_id_val,amount_val,cur_value,oper_date,src_card,comm_val,oper_type);
-
-    SELECT max(id) INTO last_id FROM entertainment;
-
-    RETURN last_id;
 
 END;
 $$;
