330d13afc0103b75eb12a0e576f46505c5ad424b
Denys Shabelnyk <dionisiy1986@gmail.com>
2025-02-03 20:21:51 +0100

    #101 - new i_groceries without return type

--- src/db/sources/func_i_groceries.sql
+++ src/db/sources/func_i_groceries.sql
@@ -1,21 +1,16 @@
-create function i_groceries(gtype_value integer, amount_value integer, currency_value integer, oper_date date, oper_type character, src_payment_card integer, comm_value character) returns integer
+create function i_groceries(gtype_value integer, amount_value integer, currency_value integer, oper_date date, oper_type character, src_payment_card integer, comm_value character) returns void
     language plpgsql
 as
 $$
-DECLARE
-    last_id int;
-
 BEGIN
 
     INSERT INTO groceries(g_type, amount, currency, date, source_card, comments,opertype)
     VALUES (gtype_value, amount_value,currency_value,oper_date,src_payment_card,comm_value,oper_type);
 
-    SELECT max(id) INTO last_id FROM groceries;
-
-    RETURN last_id;
-
 END;
 $$;
 
 alter function i_groceries(integer, integer, integer, date, char, integer, char) owner to u4cg7fn2s82n4v;
+
+
 
