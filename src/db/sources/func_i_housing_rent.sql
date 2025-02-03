443a5fa5f21a5b6aaf8659665cb993be72935a53
Denys Shabelnyk <dionisiy1986@gmail.com>
2025-02-03 20:37:39 +0100

    #101 - new i_housing_rent without return type

--- src/db/sources/func_i_housing_rent.sql
+++ src/db/sources/func_i_housing_rent.sql
@@ -1,20 +1,15 @@
-create function i_housing_rent(hr_type_id integer, amount_val integer, cur_val integer, oper_date date, oper_type character, src_card integer, comm_value character) returns integer
+create function i_housing_rent(hr_type_id integer, amount_val integer, cur_val integer, oper_date date, oper_type character, src_card integer, comm_value character) returns void
     language plpgsql
 as
 $$
-DECLARE
-    last_id int;
 
 BEGIN
 
     INSERT INTO housing_rent(hr_type_id,amount,currency,date,source_card,comments,opertype)
     VALUES (hr_type_id,amount_val,cur_val,oper_date,src_card,comm_value,oper_type);
 
-    SELECT max(id) INTO last_id FROM housing_rent;
-
-    RETURN last_id;
-
 END;
 $$;
 
 alter function i_housing_rent(integer, integer, integer, date, char, integer, char) owner to u4cg7fn2s82n4v;
+
