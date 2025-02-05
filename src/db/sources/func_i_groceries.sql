ce09ba6914fd0622079bdaeaa5856d1afca88771
Denys Shabelnyk <dionisiy1986@gmail.com>
2025-02-05 21:40:58 +0100

    #102 - new version of function with cash loging(groceries)

--- src/db/sources/func_i_groceries.sql
+++ src/db/sources/func_i_groceries.sql
@@ -1,16 +1,24 @@
+-- auto-generated definition
 create function i_groceries(gtype_value integer, amount_value integer, currency_value integer, oper_date date, oper_type character, src_payment_card integer, comm_value character) returns void
     language plpgsql
 as
 $$
 BEGIN
 
-    INSERT INTO groceries(g_type, amount, currency, date, source_card, comments,opertype)
-    VALUES (gtype_value, amount_value,currency_value,oper_date,src_payment_card,comm_value,oper_type);
+    IF oper_type = 'Cash' THEN
+        INSERT INTO cash_operations_log(optype, amount, date, comments)
+        VALUES (0,amount_value,oper_date,comm_value);
+    ELSE
+        INSERT INTO groceries(g_type, amount, currency, date, source_card, comments,opertype)
+        VALUES (gtype_value, amount_value,currency_value,oper_date,src_payment_card,comm_value,oper_type);
+    END IF;
 
 END;
 $$;
 
 alter function i_groceries(integer, integer, integer, date, char, integer, char) owner to u4cg7fn2s82n4v;
+
+
 
 
 
