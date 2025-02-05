659950aaf992ea600b184069adbf6f193f958503
Denys Shabelnyk <dionisiy1986@gmail.com>
2025-02-05 21:31:53 +0100

    #102 - new version of function with cash loging

--- src/db/sources/func_i_entertainment.sql
+++ src/db/sources/func_i_entertainment.sql
@@ -5,11 +5,18 @@ $$
 
 BEGIN
 
-    INSERT INTO entertainment(event_type_id,amount,currency,date,source_card,comments,opertype)
-    VALUES (evn_type_id_val,amount_val,cur_value,oper_date,src_card,comm_val,oper_type);
+    IF oper_type = 'Cash' THEN
+        INSERT INTO cash_operations_log(optype, amount, date, comments)
+        VALUES (0,amount_val,oper_date,comm_val);
+    ELSE
+        INSERT INTO entertainment(event_type_id,amount,currency,date,source_card,comments,opertype)
+        VALUES (evn_type_id_val,amount_val,cur_value,oper_date,src_card,comm_val,oper_type);
+    END IF;
 
 END;
 $$;
 
 alter function i_entertainment(integer, integer, integer, date, char, integer, char) owner to u4cg7fn2s82n4v;
+
+
 
