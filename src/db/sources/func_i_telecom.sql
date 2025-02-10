a287b5c83c3eaaeef94a11d4e9cdfd016f79ad39
Denys Shabelnyk <dionisiy1986@gmail.com>
2025-02-10 20:58:20 +0100

    #102 - new version of function with cash loging(telecom)

--- src/db/sources/func_i_telecom.sql
+++ src/db/sources/func_i_telecom.sql
@@ -5,13 +5,20 @@ $$
 
 BEGIN
 
-    INSERT INTO telecom(t_type_id,amount,currency,date,source_card,comments,opertype)
-    VALUES (t_type_id_val,amount_val,cur_val,oper_date,src_card,comm_val,oper_type);
+    IF oper_type = 'Cash' THEN
+        INSERT INTO cash_operations_log(optype, amount, date, comments)
+        VALUES (0,amount_val,oper_date,comm_val);
+    ELSE
+        INSERT INTO telecom(t_type_id,amount,currency,date,source_card,comments,opertype)
+        VALUES (t_type_id_val,amount_val,cur_val,oper_date,src_card,comm_val,oper_type);
+    END IF;
 
 END;
 $$;
 
 alter function i_telecom(integer, integer, integer, date, char, integer, char) owner to u4cg7fn2s82n4v;
+
+
 
 
 
