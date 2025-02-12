23859c1ba5c06a3f6a03fbfa155935217df9f0f7
Denys Shabelnyk <dionisiy1986@gmail.com>
2025-02-12 21:21:20 +0100

    #104 - func i_groceries with cash_balance mins

--- src/db/sources/func_i_groceries.sql
+++ src/db/sources/func_i_groceries.sql
@@ -1,22 +1,25 @@
--- auto-generated definition
-create function i_groceries(gtype_value integer, amount_value integer, currency_value integer, oper_date date, oper_type character, src_payment_card integer, comm_value character) returns void
-    language plpgsql
-as
-$$
+-- DROP FUNCTION public.i_groceries(int4, int4, int4, date, bpchar, int4, bpchar);
+
+CREATE OR REPLACE FUNCTION public.i_groceries(gtype_value integer, amount_value integer, currency_value integer, oper_date date, oper_type character, src_payment_card integer, comm_value character)
+ RETURNS void
+ LANGUAGE plpgsql
+AS $function$
 BEGIN
 
     IF oper_type = 'Cash' THEN
         INSERT INTO cash_operations_log(optype, amount, date, comments)
         VALUES (0,amount_value,oper_date,comm_value);
+
+    PERFORM minus_cash_balance(amount_value);
+
     ELSE
         INSERT INTO groceries(g_type, amount, currency, date, source_card, comments,opertype)
         VALUES (gtype_value, amount_value,currency_value,oper_date,src_payment_card,comm_value,oper_type);
     END IF;
 
 END;
-$$;
-
-alter function i_groceries(integer, integer, integer, date, char, integer, char) owner to u4cg7fn2s82n4v;
+$function$
+;
 
 
 
