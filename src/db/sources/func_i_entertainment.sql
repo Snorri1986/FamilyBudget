6506f40fbe398eb4718977d02e0d9a1868a106fb
Denys Shabelnyk <dionisiy1986@gmail.com>
2025-02-12 21:16:51 +0100

    #104 - func i_entertainment with cash_balance mins

--- src/db/sources/func_i_entertainment.sql
+++ src/db/sources/func_i_entertainment.sql
@@ -1,22 +1,26 @@
-create function i_entertainment(evn_type_id_val integer, amount_val integer, cur_value integer, oper_date date, oper_type character, src_card integer, comm_val character) returns void
-    language plpgsql
-as
-$$
+-- DROP FUNCTION public.i_entertainment(int4, int4, int4, date, bpchar, int4, bpchar);
+
+CREATE OR REPLACE FUNCTION public.i_entertainment(evn_type_id_val integer, amount_val integer, cur_value integer, oper_date date, oper_type character, src_card integer, comm_val character)
+ RETURNS void
+ LANGUAGE plpgsql
+AS $function$
 
 BEGIN
 
     IF oper_type = 'Cash' THEN
         INSERT INTO cash_operations_log(optype, amount, date, comments)
         VALUES (0,amount_val,oper_date,comm_val);
+
+    PERFORM minus_cash_balance(amount_val);
+
     ELSE
         INSERT INTO entertainment(event_type_id,amount,currency,date,source_card,comments,opertype)
         VALUES (evn_type_id_val,amount_val,cur_value,oper_date,src_card,comm_val,oper_type);
     END IF;
 
 END;
-$$;
-
-alter function i_entertainment(integer, integer, integer, date, char, integer, char) owner to u4cg7fn2s82n4v;
+$function$
+;
 
 
 
