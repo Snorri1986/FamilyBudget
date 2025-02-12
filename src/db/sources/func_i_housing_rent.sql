b0e222736c5389579556bc05862e50ae9b44d694
Denys Shabelnyk <dionisiy1986@gmail.com>
2025-02-12 21:31:40 +0100

    #104 - func i_housing_rent with cash_balance mins

--- src/db/sources/func_i_housing_rent.sql
+++ src/db/sources/func_i_housing_rent.sql
@@ -1,22 +1,26 @@
-create function i_housing_rent(hr_type_id integer, amount_val integer, cur_val integer, oper_date date, oper_type character, src_card integer, comm_value character) returns void
-    language plpgsql
-as
-$$
+-- DROP FUNCTION public.i_housing_rent(int4, int4, int4, date, bpchar, int4, bpchar);
+
+CREATE OR REPLACE FUNCTION public.i_housing_rent(hr_type_id integer, amount_val integer, cur_val integer, oper_date date, oper_type character, src_card integer, comm_value character)
+ RETURNS void
+ LANGUAGE plpgsql
+AS $function$
 
 BEGIN
 
     IF oper_type = 'Cash' THEN
         INSERT INTO cash_operations_log(optype, amount, date, comments)
         VALUES (0,amount_val,oper_date,comm_value);
+
+    PERFORM minus_cash_balance(amount_val);
+
     ELSE
         INSERT INTO housing_rent(hr_type_id,amount,currency,date,source_card,comments,opertype)
         VALUES (hr_type_id,amount_val,cur_val,oper_date,src_card,comm_value,oper_type);
     END IF;
 
 END;
-$$;
-
-alter function i_housing_rent(integer, integer, integer, date, char, integer, char) owner to u4cg7fn2s82n4v;
+$function$
+;
 
 
 
