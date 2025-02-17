77cb1f8e14e54389bd47a45ae16f420531380208
Denys Shabelnyk <dionisiy1986@gmail.com>
2025-02-17 20:21:56 +0100

    #98 - web page and controller

--- src/main/java/org/snorri1986/familybud/controller/AdditionalFuncPageController.java
+++ src/main/java/org/snorri1986/familybud/controller/AdditionalFuncPageController.java
@@ -1,14 +1,21 @@
 package org.snorri1986.familybud.controller;
 
+import org.snorri1986.familybud.models.AtmModelWeb;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 public class AdditionalFuncPageController {
 
   @GetMapping("/atmCash")
   public String getAtmCashPage(Model model) {
+    List<String> opList = Arrays.asList("Income", "Expenses");
+    model.addAttribute("atm_mod_attribute", new AtmModelWeb());
+    model.addAttribute("opList", opList);
     return "atm_cash";
   }
 }
