86b1d341260c4874823ee4907dfb7b5395feb0de
Denys Shabelnyk <dionisiy1986@gmail.com>
2025-03-17 21:34:34 +0100

    #111 - get_last_ten_cash_oper

---
+++ src/db/sources/get_last_ten_cash_oper.sql
@@ -0,0 +1,14 @@
+CREATE OR REPLACE FUNCTION public.get_last_ten_cash_oper()
+	RETURNS table(id int8, optype int8,amount int8,date timestamptz,comments varchar(255))
+	LANGUAGE plpgsql
+AS $function$
+	BEGIN
+          RETURN QUERY
+          SELECT c.id,c.optype,c.amount,c.date,c.comments
+          FROM public.cash_operations_log c
+          WHERE c.user_last_session = get_last_login()
+          ORDER BY i.date DESC
+          LIMIT 10;
+	END;
+$function$
+;
