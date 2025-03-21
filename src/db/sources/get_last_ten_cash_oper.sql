88aa60d15e98936aa91e51e96d500fd3340436a2
Denys Shabelnyk <dionisiy1986@gmail.com>
2025-03-21 20:50:03 +0100

    #111 - fix fields

--- src/db/sources/get_last_ten_cash_oper.sql
+++ src/db/sources/get_last_ten_cash_oper.sql
@@ -1,10 +1,10 @@
 CREATE OR REPLACE FUNCTION public.get_last_ten_cash_oper()
-	RETURNS table(id int8, optype int8,amount int8,date timestamptz,comments varchar(255))
-	LANGUAGE plpgsql
+ RETURNS TABLE(optype bigint, amount bigint, date timestamp with time zone, comments character varying)
+ LANGUAGE plpgsql
 AS $function$
 	BEGIN
           RETURN QUERY
-          SELECT c.id,c.optype,c.amount,c.date,c.comments
+          SELECT c.optype,c.amount,c.date,c.comments
           FROM public.cash_operations_log c
           WHERE c.user_last_session = get_last_login()
           ORDER BY c.date DESC
@@ -12,3 +12,4 @@ AS $function$
 	END;
 $function$
 ;
+;;
