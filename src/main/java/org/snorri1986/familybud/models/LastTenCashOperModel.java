6f2ed56336f7f4e81dd7038a2ab559933ac9b48d
Denys Shabelnyk <dionisiy1986@gmail.com>
2025-03-24 19:47:02 +0100

    #111 - last ten cash operations model

---
+++ src/main/java/org/snorri1986/familybud/models/LastTenCashOperModel.java
@@ -0,0 +1,59 @@
+package org.snorri1986.familybud.models;
+
+import java.time.ZonedDateTime;
+
+public class LastTenCashOperModel {
+  private long opType;
+  private long amount;
+  private ZonedDateTime date;
+  private String comments;
+
+  public LastTenCashOperModel(long opType, long amount, ZonedDateTime date, String comments) {
+    this.opType = opType;
+    this.amount = amount;
+    this.date = date;
+    this.comments = comments;
+  }
+
+  public long getOpType() {
+    return opType;
+  }
+
+  public void setOpType(long opType) {
+    this.opType = opType;
+  }
+
+  public long getAmount() {
+    return amount;
+  }
+
+  public void setAmount(long amount) {
+    this.amount = amount;
+  }
+
+  public ZonedDateTime getDate() {
+    return date;
+  }
+
+  public void setDate(ZonedDateTime date) {
+    this.date = date;
+  }
+
+  public String getComments() {
+    return comments;
+  }
+
+  public void setComments(String comments) {
+    this.comments = comments;
+  }
+
+  @Override
+  public String toString() {
+    return "LastTenCashOperModel{" +
+            "opType=" + opType +
+            ", amount=" + amount +
+            ", date=" + date +
+            ", comments='" + comments + '\'' +
+            '}';
+  }
+}
