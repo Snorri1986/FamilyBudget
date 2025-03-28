cdcadd0a4c7c67bb51e9e13624ebec1a666a8bfc
Denys Shabelnyk <dionisiy1986@gmail.com>
2025-03-28 20:20:03 +0100

    #111 - new method on DB service for Entertainment

--- src/main/java/org/snorri1986/familybud/service/DBService.java
+++ src/main/java/org/snorri1986/familybud/service/DBService.java
@@ -149,6 +149,22 @@ public class DBService {
       }
     });
   }
+
+  public List<LastTenEntertainmentModel> getLastTenEntertainmentOperations() {
+    String sql = "SELECT * FROM get_last_ten_entertainment()";
+    return jdbcTemplate.query(sql, new RowMapper<LastTenEntertainmentModel>() {
+      @Override
+      public LastTenEntertainmentModel mapRow(ResultSet rs,int rowNum) throws SQLException {
+        return new LastTenEntertainmentModel(rs.getLong("event_type_id"),
+                rs.getLong("amount"),
+                rs.getLong("currency"),
+                rs.getTimestamp("date").toInstant().atZone(ZoneId.systemDefault()),
+                rs.getLong("source_card"),
+                rs.getString("operType"),
+                rs.getString("comments"));
+      }
+    });
+  }
 }
 
 
