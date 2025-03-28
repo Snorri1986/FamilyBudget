5a09660181adc4c5bd698b5d1c06d5a3f6e66a4d
Denys Shabelnyk <dionisiy1986@gmail.com>
2025-03-28 21:00:35 +0100

    #111 - db function for groceries

---
+++ src/db/sources/get_last_ten_groceries_oper.sql
@@ -0,0 +1,20 @@
+CREATE OR REPLACE FUNCTION public.get_last_ten_groceries_oper()
+ RETURNS TABLE(g_type bigint, amount bigint, currency bigint, date timestamp with time zone, source_card bigint, opertype character varying, comments character varying)
+ LANGUAGE plpgsql
+AS $function$
+	BEGIN
+         RETURN QUERY
+          SELECT g.g_type,
+                 g.amount,
+                 g.currency,
+                 g.date,
+                 g.source_card,
+                 g.opertype,
+                 g.comments
+          FROM public.groceries g
+          WHERE g.user_last_session = get_last_login()
+          ORDER BY g.date DESC
+          LIMIT 10;
+	END;
+$function$
+;
