e1f7b672aaedcab4645fc806c85f1f50a7a26b10
Denys Shabelnyk <dionisiy1986@gmail.com>
2025-03-31 20:13:44 +0200

    #111 - model for rentHousing

---
+++ src/main/java/org/snorri1986/familybud/models/LastTenHousingRentOperations.java
@@ -0,0 +1,92 @@
+package org.snorri1986.familybud.models;
+
+import java.time.ZonedDateTime;
+
+public class LastTenHousingRentOperations {
+  private long housingRentType;
+  private long amount;
+  private long currency;
+  private ZonedDateTime date;
+  private long sourceCard;
+  private String operType;
+  private String comments;
+
+  public LastTenHousingRentOperations(long housingRentType, long amount, long currency, ZonedDateTime date, long sourceCard, String operType, String comments) {
+    this.housingRentType = housingRentType;
+    this.amount = amount;
+    this.currency = currency;
+    this.date = date;
+    this.sourceCard = sourceCard;
+    this.operType = operType;
+    this.comments = comments;
+  }
+
+  public long getHousingRentType() {
+    return housingRentType;
+  }
+
+  public void setHousingRentType(long housingRentType) {
+    this.housingRentType = housingRentType;
+  }
+
+  public long getAmount() {
+    return amount;
+  }
+
+  public void setAmount(long amount) {
+    this.amount = amount;
+  }
+
+  public long getCurrency() {
+    return currency;
+  }
+
+  public void setCurrency(long currency) {
+    this.currency = currency;
+  }
+
+  public ZonedDateTime getDate() {
+    return date;
+  }
+
+  public void setDate(ZonedDateTime date) {
+    this.date = date;
+  }
+
+  public long getSourceCard() {
+    return sourceCard;
+  }
+
+  public void setSourceCard(long sourceCard) {
+    this.sourceCard = sourceCard;
+  }
+
+  public String getOperType() {
+    return operType;
+  }
+
+  public void setOperType(String operType) {
+    this.operType = operType;
+  }
+
+  public String getComments() {
+    return comments;
+  }
+
+  public void setComments(String comments) {
+    this.comments = comments;
+  }
+
+  @Override
+  public String toString() {
+    return "LastTenHousingRentOperations{" +
+            "housingRentType=" + housingRentType +
+            ", amount=" + amount +
+            ", currency=" + currency +
+            ", date=" + date +
+            ", sourceCard=" + sourceCard +
+            ", operType='" + operType + '\'' +
+            ", comments='" + comments + '\'' +
+            '}';
+  }
+}
