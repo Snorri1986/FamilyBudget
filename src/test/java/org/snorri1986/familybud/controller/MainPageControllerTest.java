81b4650b5572d4687bce7a32d655eabc60ee1fc7
Denys Shabelnyk <dionisiy1986@gmail.com>
2025-04-11 21:19:07 +0200

    #119 - verify last ten testGetEntertainmentPage

--- src/test/java/org/snorri1986/familybud/controller/MainPageControllerTest.java
+++ src/test/java/org/snorri1986/familybud/controller/MainPageControllerTest.java
@@ -59,12 +59,16 @@ public class MainPageControllerTest {
 
   @Test
   void testGetEntertainmentPage() {
+    List<LastTenEntertainmentModel> mockLastTenEntertainment = List.of(new LastTenEntertainmentModel(), new LastTenEntertainmentModel());
+    when(dbService.getLastTenEntertainmentOperations()).thenReturn(mockLastTenEntertainment);
     String viewName = mainPageController.getEntertainmentPage(model);
     assertEquals("entertainment", viewName);
     verify(model).addAttribute(eq("entertainment_mod_attribute"), any(EntertainmentModelWeb.class));
     verify(model).addAttribute(eq("entList"), any(List.class));
     verify(model).addAttribute(eq("currencyNames"),any(List.class));
     verify(model).addAttribute(eq("transactionTypes"),any(List.class));
+    verify(model).addAttribute("entertainmentLastTen", mockLastTenEntertainment);
+    verifyNoMoreInteractions(model);
   }
 
   @Test
