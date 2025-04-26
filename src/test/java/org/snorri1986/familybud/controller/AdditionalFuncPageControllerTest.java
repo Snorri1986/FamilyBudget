212ada3dbabb323a4247e4843be466268c9130b7
Denys Shabelnyk <dionisiy1986@gmail.com>
2025-04-26 13:00:23 +0200

    #119 - mock dbService.

--- src/test/java/org/snorri1986/familybud/controller/AdditionalFuncPageControllerTest.java
+++ src/test/java/org/snorri1986/familybud/controller/AdditionalFuncPageControllerTest.java
@@ -5,12 +5,16 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.snorri1986.familybud.service.DBService;
 import org.springframework.ui.Model;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @ExtendWith(MockitoExtension.class)
 public class AdditionalFuncPageControllerTest {
+
+  @Mock
+  DBService dbService;
 
   @InjectMocks
   private AdditionalFuncPageController additionalFuncPageController;
@@ -20,7 +24,6 @@ public class AdditionalFuncPageControllerTest {
 
   @Test
   void testGetAtmCashPage() {
-    //TODO: look at MainPageControllerTest
     String viewName = additionalFuncPageController.getAtmCashPage(model);
     assertEquals("atm_cash", viewName);
   }
