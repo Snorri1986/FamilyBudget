05262cab7695cb8cf214e239d9e94e7a224c963e
Denys Shabelnyk <dionisiy1986@gmail.com>
2025-07-28 20:55:09 +0200

    #144 - new DB function get_last_ten_health_oper_cash()

---
+++ src/db/sources/get_last_ten_health_oper_cash.sql
@@ -0,0 +1,22 @@
+CREATE OR REPLACE FUNCTION public.get_last_ten_health_oper_cash()
+	RETURNS TABLE(h_type_id int, amount bigint, currency bigint,
+	              date timestamp with time zone, opertype text,
+	              comments varchar(255))
+	LANGUAGE plpgsql
+AS $function$
+	BEGIN
+         RETURN QUERY
+          SELECT
+                   c_log.ex_type_id,
+                   c_log.amount,
+                   c_log.currency,
+                   c_log.date,
+                   'Cash' AS opertype,
+                   c_log.comments
+          FROM public.cash_operations_log c_log
+          WHERE c_log.user_last_session = get_last_login()
+          ORDER BY c_log.date DESC
+          LIMIT 10;
+	END;
+$function$
+;
