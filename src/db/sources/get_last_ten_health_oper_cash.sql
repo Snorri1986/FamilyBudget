7ed8d77d4b40f892b314572f89a9516ac9d18c11
Denys Shabelnyk <dionisiy1986@gmail.com>
2025-08-05 20:54:36 +0200

    #167 - new function returns only health operations

--- src/db/sources/get_last_ten_health_oper_cash.sql
+++ src/db/sources/get_last_ten_health_oper_cash.sql
@@ -1,8 +1,6 @@
 CREATE OR REPLACE FUNCTION public.get_last_ten_health_oper_cash()
-	RETURNS TABLE(h_type_id int, amount bigint, currency bigint,
-	              date timestamp with time zone, opertype text,
-	              comments varchar(255))
-	LANGUAGE plpgsql
+ RETURNS TABLE(h_type_id integer, amount bigint, currency bigint, date timestamp with time zone, opertype text, comments character varying)
+ LANGUAGE plpgsql
 AS $function$
 	BEGIN
          RETURN QUERY
@@ -15,6 +13,7 @@ AS $function$
                    c_log.comments
           FROM public.cash_operations_log c_log
           WHERE c_log.user_last_session = get_last_login()
+          AND c_log.ex_type_id IN (23,24,25,26,27,28,42,22)
           ORDER BY c_log.date DESC
           LIMIT 10;
 	END;
