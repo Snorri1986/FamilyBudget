1d38670f8e230659b2209bfe7ae73d7c23a6f027
Denys Shabelnyk <dionisiy1986@gmail.com>
2025-08-11 21:53:08 +0200

    #163 - with dbService.getLastTenIncomesCard()

--- src/main/java/org/snorri1986/familybud/controller/MainPageController.java
+++ src/main/java/org/snorri1986/familybud/controller/MainPageController.java
@@ -34,13 +34,12 @@ public class MainPageController {
   @GetMapping("/income")
   public String getIncomePage(Model model) {
     List<String> incomes = Arrays.asList("Salary", "Bonus", "TravelRefund", "ShopRefund","Money transfer R", "Other");
-    //List<LastTenIncomesModel> lastTenIncomes = dbService.getLastTenIncomes();
     List<LastTenIncomesModel> lastTenIncomesCard = dbService.getLastTenIncomesCard();
     model.addAttribute("income_mod_attribute", new IncomeModelWeb());
     model.addAttribute("incomes", incomes);
     model.addAttribute("currencyNames", currencyNames);
     model.addAttribute("transactionTypes", transactionTypes);
-    model.addAttribute("incomesLastTen", lastTenIncomesCard);
+    model.addAttribute("incomesLastTenCard", lastTenIncomesCard);
     return "income";
   }
 
