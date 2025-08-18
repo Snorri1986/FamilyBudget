ab345a44d823f354d26bb06736710fd8df69e8ce
Denys Shabelnyk <dionisiy1986@gmail.com>
2025-08-18 23:21:41 +0200

    #174 - on main controller level

--- src/main/java/org/snorri1986/familybud/controller/MainPageController.java
+++ src/main/java/org/snorri1986/familybud/controller/MainPageController.java
@@ -48,13 +48,12 @@ public class MainPageController {
   @GetMapping("/entertainment")
   public String getEntertainmentPage(Model model) {
     List<String> entList = Arrays.asList("Travel", "Cinema", "Vacation", "Relax","Restaurant","Homefest","Other");
-    //List<LastTenEntertainmentModel> lastTenEntertainment = dbService.getLastTenEntertainmentOperations();
     List<LastTenEntertainmentModel> lastTenEntertainmentCard = dbService.getLastTenEntertainmentOperationsCard();
     model.addAttribute("entertainment_mod_attribute", new EntertainmentModelWeb());
     model.addAttribute("entList", entList);
     model.addAttribute("currencyNames", currencyNames);
     model.addAttribute("transactionTypes", transactionTypes);
-    model.addAttribute("entertainmentLastTen", lastTenEntertainmentCard);
+    model.addAttribute("entertainmentLastTenCard", lastTenEntertainmentCard);
     return "entertainment";
   }
 
