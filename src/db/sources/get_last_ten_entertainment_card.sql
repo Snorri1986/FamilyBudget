6bd546bf23c5b67e16a2016c14e4e8739bcb1f9e
Denys Shabelnyk <dionisiy1986@gmail.com>
2025-08-18 22:36:35 +0200

    #170 - new function only for card operations

---
+++ src/db/sources/get_last_ten_entertainment_card.sql
@@ -0,0 +1,21 @@
+CREATE OR REPLACE FUNCTION public.get_last_ten_entertainment_card()
+	RETURNS TABLE(event_type_id bigint, amount bigint, currency bigint, date timestamp with time zone, source_card bigint, opertype text, comments character varying)
+	LANGUAGE plpgsql
+AS $function$
+	BEGIN
+         RETURN QUERY
+          SELECT e.event_type_id,
+                 e.amount,
+                 e.currency,
+                 e.date,
+                 e.source_card,
+                 e.opertype,
+                 e.comments
+          FROM public.entertainment e
+          WHERE e.user_last_session = get_last_login()
+          AND e.opertype = 'Card'
+          ORDER BY e.date DESC
+          LIMIT 10;
+     END;
+$function$
+;
