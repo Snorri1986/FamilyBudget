0d0a06e74fc155ec1912486d17c6afc292a3c99f
Denys Shabelnyk <dionisiy1986@gmail.com>
2025-09-15 23:04:41 +0200

    #194 - new attributes

--- src/main/java/org/snorri1986/familybud/controller/MainPageController.java
+++ src/main/java/org/snorri1986/familybud/controller/MainPageController.java
@@ -90,14 +90,14 @@ public class MainPageController {
   @GetMapping("/renthousing")
   public String getRentHousingPage(Model model) {
     List<String> housingTypes = Arrays.asList("Rent","Mortage","Money transfer S","A-kass","Electricity","HouseEquipments","Renovation","Other");
-    //List<LastTenHousingRentOperationsModel> lastTenRentHousing = dbService.getLastTenHousingRentOperations();
     List<LastTenHousingRentOperationsModel> lastTenRentHousingCard = dbService.getLastTenHousingRentOperationsCard();
+    List<LastTenHousingRentOperationsModel> lastTenRentHousingCash = dbService.getLastTenHousingRentOperationsCash();
     model.addAttribute("rent_housing_mod_attribute", new RentHousingModelWeb());
     model.addAttribute("housingTypes", housingTypes);
     model.addAttribute("currencyNames", currencyNames);
     model.addAttribute("transactionTypes", transactionTypes);
-    //model.addAttribute("rentHousingLastTen", lastTenRentHousing);
     model.addAttribute("rentHousingLastTenCard", lastTenRentHousingCard);
+    model.addAttribute("rentHousingLastTenCash", lastTenRentHousingCash);
     return "renthousing";
   }
 
