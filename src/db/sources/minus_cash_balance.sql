8c834c3bfe4fd72314e2d1cfb0aad25eacdb0fb1
Denys Shabelnyk <dionisiy1986@gmail.com>
2025-11-01 12:02:56 +0100

    #221 - balance protection is not necessary

--- src/db/sources/minus_cash_balance.sql
+++ src/db/sources/minus_cash_balance.sql
@@ -2,14 +2,8 @@ CREATE OR REPLACE FUNCTION public.minus_cash_balance(amount_value integer)
  RETURNS void
  LANGUAGE plpgsql
 AS $function$
-    DECLARE
-          current_cash_balance int4;
-	BEGIN
-         SELECT balance INTO current_cash_balance FROM cash_balance;
-
-         IF amount_value <= current_cash_balance THEN
-               UPDATE cash_balance SET balance = balance - amount_value;
-         END IF;
-	END;
+    BEGIN
+         UPDATE cash_balance SET balance = balance - amount_value;
+    END;
 $function$
 ;
