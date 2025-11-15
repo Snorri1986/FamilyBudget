f0e9c1773584a32252ee67ea00b6765d8d1e9237
Denys Shabelnyk <dionisiy1986@gmail.com>
2025-11-15 13:55:17 +0100

    #226 - new model

--- src/main/java/org/snorri1986/familybud/models/LastTenAtmOperationsModel.java
+++ src/main/java/org/snorri1986/familybud/models/LastTenAtmOperationsModel.java
@@ -2,18 +2,17 @@ package org.snorri1986.familybud.models;
 
 import org.springframework.format.annotation.DateTimeFormat;
 
-import java.util.Date;
+import java.time.ZonedDateTime;
 
 public class LastTenAtmOperationsModel {
-  private String operType;
-  private int amount;
+  private long operType;
+  private long amount;
   @DateTimeFormat(pattern = "dd.MM.yyyy HH:mm")
-  private Date transactionDate;
-  private int currency;
+  private ZonedDateTime transactionDate;
+  private long currency;
   private String operDescription;
 
-  // constructor
-  public LastTenAtmOperationsModel(String operType, int amount, Date transactionDate, int currency, String operDescription) {
+  public LastTenAtmOperationsModel(long operType, long amount, ZonedDateTime transactionDate, long currency, String operDescription) {
     this.operType = operType;
     this.amount = amount;
     this.transactionDate = transactionDate;
@@ -21,41 +20,39 @@ public class LastTenAtmOperationsModel {
     this.operDescription = operDescription;
   }
 
-  // getters
-  public String getOperType() {
+  public long getOperType() {
     return operType;
   }
 
-  public int getAmount() {
+  public long getAmount() {
     return amount;
   }
 
-  public int getCurrency() {
-    return currency;
+  public ZonedDateTime getTransactionDate() {
+    return transactionDate;
   }
 
-  public Date getTransactionDate() {
-    return transactionDate;
+  public long getCurrency() {
+    return currency;
   }
 
   public String getOperDescription() {
     return operDescription;
   }
 
-  // setters
-  public void setOperType(String operType) {
+  public void setOperType(long operType) {
     this.operType = operType;
   }
 
-  public void setAmount(int amount) {
+  public void setAmount(long amount) {
     this.amount = amount;
   }
 
-  public void setTransactionDate(Date transactionDate) {
+  public void setTransactionDate(ZonedDateTime transactionDate) {
     this.transactionDate = transactionDate;
   }
 
-  public void setCurrency(int currency) {
+  public void setCurrency(long currency) {
     this.currency = currency;
   }
 
@@ -66,7 +63,7 @@ public class LastTenAtmOperationsModel {
   @Override
   public String toString() {
     return "LastTenAtmOperationsModel{" +
-            "operType='" + operType + '\'' +
+            "operType=" + operType +
             ", amount=" + amount +
             ", transactionDate=" + transactionDate +
             ", currency=" + currency +
