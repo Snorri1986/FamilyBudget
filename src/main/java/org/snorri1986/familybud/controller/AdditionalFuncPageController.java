c6f67408acbc02d66d3331d6a23076abae2fa770
Denys Shabelnyk <dionisiy1986@gmail.com>
2025-11-22 14:15:09 +0100

    #228 - remove old method

--- src/main/java/org/snorri1986/familybud/controller/AdditionalFuncPageController.java
+++ src/main/java/org/snorri1986/familybud/controller/AdditionalFuncPageController.java
@@ -2,7 +2,6 @@ package org.snorri1986.familybud.controller;
 
 import org.snorri1986.familybud.models.AtmModelWeb;
 import org.snorri1986.familybud.models.LastTenAtmOperationsModel;
-import org.snorri1986.familybud.models.LastTenCashOperModel;
 import org.snorri1986.familybud.service.DBService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -21,20 +20,9 @@ public class AdditionalFuncPageController {
   @Autowired
   DBService dbService;
 
-  /*@GetMapping("/atmCash")
-  public String getAtmCashPage(Model model) {
-    List<String> opList = Arrays.asList("Income", "Expenses");
-    List<LastTenCashOperModel> lastTenCashOperations = dbService.getLastTenCashOperations();
-    model.addAttribute("atm_mod_attribute", new AtmModelWeb());
-    model.addAttribute("opList", opList);
-    model.addAttribute("lastTenCashOperations", lastTenCashOperations);
-    return "atm_cash";
-  }*/
-
   @GetMapping("/atmCash")
   public String getLastTenAtmCashOperations(Model model) {
     List<String> opList = Arrays.asList("Income", "Expenses");
-    //List<LastTenCashOperModel> lastTenCashOperations = dbService.getLastTenCashOperations();
     List<LastTenAtmOperationsModel> lastTenAtmOperationsModel = dbService.getLastTenAtmOperations();
     model.addAttribute("atm_mod_attribute", new AtmModelWeb());
     model.addAttribute("opList", opList);
