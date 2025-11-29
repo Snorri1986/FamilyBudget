ecc322b87aa074af658a11915288a5cf745489a8
Denys Shabelnyk <dionisiy1986@gmail.com>
2025-11-29 13:44:55 +0100

    #232 - new version with code 18

--- src/db/sources/get_last_ten_incomes_cash.sql
+++ src/db/sources/get_last_ten_incomes_cash.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE FUNCTION public.get_last_ten_health_oper_cash()
+CREATE OR REPLACE FUNCTION public.get_last_ten_incomes_cash()
  RETURNS TABLE(ex_type_id integer, amount bigint, currency bigint, date timestamp with time zone, opertype text, comments character varying)
  LANGUAGE plpgsql
 AS $function$
@@ -13,7 +13,7 @@ AS $function$
                    c_log.comments
           FROM public.cash_operations_log c_log
           WHERE c_log.user_last_session = get_last_login()
-          AND c_log.ex_type_id IN (23,24,25,26,27,28,42,22)
+          AND c_log.ex_type_id IN (14,13,15,16,41,17,18)
           ORDER BY c_log.date DESC
           LIMIT 10;
 	END;
