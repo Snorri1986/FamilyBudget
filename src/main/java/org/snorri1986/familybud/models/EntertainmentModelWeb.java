e44e0c821aaf39b45c240e62ae8aab7eed5ca1c7
Denys Shabelnyk <dionisiy1986@gmail.com>
2025-12-18 21:50:07 +0100

    #239 - 2 new constructors for EntertainmentModelWeb

--- src/main/java/org/snorri1986/familybud/models/EntertainmentModelWeb.java
+++ src/main/java/org/snorri1986/familybud/models/EntertainmentModelWeb.java
@@ -14,6 +14,12 @@ public class EntertainmentModelWeb {
   private int cardNum;
   private String operDescription;
 
+  public EntertainmentModelWeb() {}
+
+  public EntertainmentModelWeb(int cardNum) {
+    this.cardNum = cardNum;
+  }
+
   public String getEventType() {
     return eventType;
   }
