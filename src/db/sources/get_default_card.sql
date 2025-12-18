33a483ab76c828bf7b32662407114a6576a53cb3
Denys Shabelnyk <dionisiy1986@gmail.com>
2025-12-18 21:39:02 +0100

    #239 - new return type

--- src/db/sources/get_default_card.sql
+++ src/db/sources/get_default_card.sql
@@ -1,14 +1,14 @@
 CREATE OR REPLACE FUNCTION public.get_default_card()
-	RETURNS text
+	RETURNS integer
 	LANGUAGE plpgsql
 AS $function$
-    DECLARE
-           card_default varchar;
+	DECLARE
+           card_default integer;
 	BEGIN
-           SELECT users.card_default INTO card_default FROM public.users
+           SELECT users.card_default::integer
+           INTO card_default FROM public.users
            WHERE username = public.get_last_login();
-
            RETURN card_default;
-	END;
+    END;
 $function$
 ;
