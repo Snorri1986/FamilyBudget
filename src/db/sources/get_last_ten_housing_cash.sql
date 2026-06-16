29505f3a1ebf4f8be94e1b82a56a7188f0ab71bc
Denys Shabelnyk <dionisiy1986@gmail.com>
2026-06-16 20:30:49 +0200

    #411 - new version via table public.cash_expense_id

--- src/db/sources/get_last_ten_housing_cash.sql
+++ src/db/sources/get_last_ten_housing_cash.sql
@@ -1,21 +1,19 @@
-CREATE OR REPLACE FUNCTION public.get_last_ten_housing_cash()
-	RETURNS TABLE(h_type_id integer, amount bigint, currency bigint, date timestamp with time zone, opertype text, comments character varying)
-	LANGUAGE plpgsql
-AS $function$
-	BEGIN
-         RETURN QUERY
-          SELECT
-                   c_log.ex_type_id,
-                   c_log.amount,
-                   c_log.currency,
-                   c_log.date,
-                   'Cash' AS opertype,
-                   c_log.comments
-          FROM public.cash_operations_log c_log
-          WHERE c_log.user_last_session = get_last_login()
-          AND c_log.ex_type_id IN (2,37,40,39,29,30,31,22)
-          ORDER BY c_log.date DESC
-          LIMIT 10;
-     END;
-$function$
-;
+create function get_last_ten_housing_cash()
+    returns TABLE(h_type_id integer, amount bigint, currency bigint, date timestamp with time zone, opertype text, comments character varying)
+    language sql
+    as
+$$
+SELECT
+    c_log.ex_type_id,
+    c_log.amount,
+    c_log.currency,
+    c_log.date,
+    'Cash'::text AS opertype,
+    c_log.comments
+FROM public.cash_operations_log AS c_log
+         JOIN public.cash_expense_id AS c_exp ON c_log.ex_type_id = c_exp.exp_cash_id
+WHERE c_log.user_last_session = public.get_last_login()
+  AND c_exp.exp_area = 'Housing'
+ORDER BY c_log.date DESC
+    LIMIT 10;
+$$;
