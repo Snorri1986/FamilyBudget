2355380afca53a68daab6a7477b47aec7eb582fb
Denys Shabelnyk <dionisiy1986@gmail.com>
2026-08-19 19:52:01 +0200

    #331 - new version with minus_card_balance call

--- src/db/sources/func_i_telecom.sql
+++ src/db/sources/func_i_telecom.sql
@@ -39,17 +39,20 @@ BEGIN
 
     vat_value := calculate_vat(amount_val);
 
-
-
     IF oper_type = 'Cash' THEN
-        INSERT INTO cash_operations_log(optype, amount, date, comments,user_last_session,currency,ex_type_id,vat)
-        VALUES (0,amount_val,oper_date,comm_val,username,cur_val,t_type_id_val,vat_value);
 
         PERFORM minus_cash_balance(amount_val);
 
+        INSERT INTO cash_operations_log(optype, amount, date, comments,user_last_session,currency,ex_type_id,vat)
+        VALUES (0,amount_val,oper_date,comm_val,username,cur_val,t_type_id_val,vat_value);
+
     ELSE
+
+        PERFORM minus_card_balance(amount_val);
+
         INSERT INTO telecom(t_type_id,amount,currency,date,source_card,comments,opertype,user_last_session,vat)
         VALUES (t_type_id_val,amount_val,cur_val,oper_date,src_card,comm_val,oper_type,username,vat_value);
+
     END IF;
 
 END;
