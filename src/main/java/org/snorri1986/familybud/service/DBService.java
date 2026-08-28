1299205f5a7c2367b05ace5958f92d45378f6987
Denys Shabelnyk <dionisiy1986@gmail.com>
2026-08-28 20:17:12 +0200

    #342 - new method getDailyVatReportByCurrency

--- src/main/java/org/snorri1986/familybud/service/DBService.java
+++ src/main/java/org/snorri1986/familybud/service/DBService.java
@@ -166,6 +166,11 @@ public class DBService {
      return jdbcTemplate.queryForObject(sql, new Object[]{currencyId}, Integer.class);
   }
 
+  public double getDailyVatReportByCurrency(int currencyId) {
+    String sql = "SELECT public.get_all_vat_daily(?)";
+    return jdbcTemplate.queryForObject(sql, new Object[]{currencyId}, Double.class);
+  }
+
   public LocationModel getLocation() {
     return jdbcTemplate.queryForObject(GET_LOCATION_SQL, (rs, rowNum) -> new LocationModel(rs.getString("user_country"),
             rs.getString("user_city")));
